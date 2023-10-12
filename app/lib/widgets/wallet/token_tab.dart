0d012a3b0366f02a24c97d790e984a84ab7a0300
Ryan Weald <ryan@weald.com>
2023-10-11 17:17:37 -0700

    Add nice UX for the token claiming flow

--- app/lib/widgets/wallet/token_tab.dart
+++ app/lib/widgets/wallet/token_tab.dart
@@ -32,59 +32,91 @@ class TokenTabState extends State<TokenTab> {
     });
   }
 
+  Future<void> claimRly() async {
+    setState(() {
+      _claimingRly = true;
+    });
+    await rlyNetwork.claimRly();
+    await getBalance();
+    setState(() {
+      _claimingRly = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (_loading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
     return Padding(
       padding: const EdgeInsets.only(top: 16.0),
-      child: _loading
-          ? const Column(
-              children: [
-                CircularProgressIndicator(),
-              ],
-            )
-          : Column(
-              children: [
-                const Text("\$RLY balance", style: TextStyle(fontSize: 18)),
-                Text(
-                  "$_balance",
-                  style: const TextStyle(fontSize: 24),
-                ),
-                ElevatedButton(
-                    onPressed: getBalance, child: const Text("Refresh")),
-                if (_balance == 0) claimRlyWidget()
-              ],
-            ),
+      child: _balance == 0 ? claimRlyWidget() : alreadyClaimedUser(),
+    );
+  }
+
+  Widget alreadyClaimedUser() {
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10.0),
+          child: Text('Welcome back!', style: TextStyle(fontSize: 20)),
+        ),
+        const Text("\$RLY balance", style: TextStyle(fontSize: 18)),
+        Text(
+          "$_balance",
+          style: const TextStyle(fontSize: 24),
+        ),
+      ],
     );
   }
 
   Widget claimRlyWidget() {
     if (_claimingRly) {
       return (const Column(
-        children: [Text("Claiming RLY..."), CircularProgressIndicator()],
+        children: [
+          Text("Claiming RLY...", style: TextStyle(fontSize: 18)),
+          Padding(
+            padding: EdgeInsets.only(top: 12.0),
+            child: CircularProgressIndicator(),
+          )
+        ],
       ));
     }
     return Padding(
-      padding: const EdgeInsets.only(top: 48),
+      padding: const EdgeInsets.only(top: 12),
       child: Column(
         children: [
           const Center(
             child: Text("Welcome New User to Rally Protocol!",
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  fontSize: 28,
+                  fontSize: 20,
                 )),
           ),
-          ElevatedButton(
-              onPressed: _loading
-                  ? null
-                  : () async {
-                      setState(() {
-                        _claimingRly = true;
-                      });
-                      await rlyNetwork.claimRly();
-                      getBalance();
-                    },
-              child: const Text("Claim Your RLY"))
+          Padding(
+            padding: const EdgeInsets.only(top: 12),
+            child: ElevatedButton(
+                onPressed: _loading
+                    ? null
+                    : () {
+                        claimRly();
+                      },
+                child: const Text("Claim ERC20")),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 12.0),
+            child: Center(
+              child: Text(
+                  "This will claim 10 units of an ERC20 contract without the user needing native tokens in their wallet",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontSize: 12.0,
+                  )),
+            ),
+          )
         ],
       ),
     );
